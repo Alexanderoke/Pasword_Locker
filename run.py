cfe7683d004512b652cd8a505556bceb224b672d
alexanderoke <alexander.oke@student.moringaschool.com>
2022-01-26 18:18:10 +0300

    run.py completed

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 from user import user
 
 
@@ -8,6 +9,26 @@ def main():
     print("write 'y'to create a new user account: write 'N' to login to your account type 'exit' to exit the system ")
     access_code=input().lower()
     print('\n')
-    
-  if__name__='__main__':
-main()
+
+    if access_code =='y':
+      print("enter username")
+      new_user_name=input()
+      print("Welcome"+' '+new_user_name+' '+"Please enter your password")
+      new_user_password=input()
+      print("Welcome"+' ' + new_user_name+' '+"account succesfully created." )
+    else:
+      access_code='N'
+      print("please login")
+      print("Enter username")
+      entered_username=input()
+      print("enter password")
+      entered_password=input()
+
+      while entered_username==new_user_name and entered_password==new_user_password:
+        print("login succesfull")
+      else:
+         access_code=='exit'
+         break
+
+  if__name__='__main__'
+  main()
--- user.py
+++ user.py
@@ -17,3 +17,10 @@ class user:
       stores new user tonew_user list
       """
       user.new_user.append(self)
+
+      def delete_user(self):
+        
+        '''
+        delete_user method deletes a saved user from the new_user
+        '''
+        user.new_user.remove(self)
